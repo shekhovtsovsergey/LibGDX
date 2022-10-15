aab6b2a9b58e728b27051582216c2c8e140fb554
shekhovtsovsergey <98221024+shekhovtsovsergey@users.noreply.github.com>
2022-10-15 18:47:32 +0300

    Initial commit

---
+++ core/src/com/mygdx/game/MyAnim.java
@@ -0,0 +1,37 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class MyAnim {
+    Texture img;
+    Animation<TextureRegion> anm;
+    private float time;
+
+    public MyAnim(String name, int row, int col, float fps, Animation.PlayMode playMode){
+        time = 0;
+        img = new Texture(name);
+        TextureRegion reg1 = new TextureRegion(img);
+        TextureRegion[][] regions = reg1.split(img.getWidth()/col, img.getHeight()/row);
+        //TextureRegion[] tmp = new TextureRegion[regions.length*regions[0].length];
+        //int cnt = 0;
+        //for (TextureRegion[] region : regions) {for (TextureRegion reg: region) tmp[cnt++] = reg;}
+        anm = new Animation<>(1/fps, regions[0]);
+        anm.setPlayMode(playMode);
+    }
+
+    public TextureRegion draw() {
+        return anm.getKeyFrame(time);
+    }
+
+    public void setTime(float dT){
+        time += dT;
+    }
+
+    public void dispose(){
+        this.img.dispose();
+    }
+
+
+}
